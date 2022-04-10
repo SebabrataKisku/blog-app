387a79a405ced00a59f763a79d07c79d0205db7c
Sebabrata Kisku <sebabrata.mid013@gmail.com>
2022-04-10 13:41:39 +0530

    forms and user accounts

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from tkinter import CASCADE
 from django.db import models
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -8,3 +9,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title[:50]
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,29 @@
 # blog/views.py
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 from .models import Post
 
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
 
-class BlogDetailView(DetailView): # new
+class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'author', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
